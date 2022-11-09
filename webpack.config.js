8f6eef2ffe6f90bb62041ff2c0ac06927d95f90c
Al <Batuten.mail@gmail.com>
2022-11-09 17:40:36 +0400

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
   devServer: {
     static: './dist',
     compress: true,
-    port: process.env.PORT ?? 4000,
+    port: process.env.PORT ?? 3000,
+    disableHostCheck: true,
     hot: true,
     open: true
   },
